fa12f64bfab6cb668a1f46d27bed6567917cc09b
Gee <fengmoxi@vip.qq.com>
2019-10-24 16:52:12 +0800

    other: 设定MySQL连接参数

--- config/mysql.go
+++ config/mysql.go
@@ -10,14 +10,14 @@ func GetDbConfig() map[string]string {
 	dbConfig["MAX_LIFETIME_CONNS"] = "7200" // 连接池链接最长生命周期
 	dbConfig["TIMEZONE"] = "Asia/Shanghai"  // 时区
 
-	dbConfig["HOST"] = "127.0.0.1"   // 主机
-	dbConfig["PORT"] = "3306"        // 端口
-	dbConfig["DATABASE"] = "main"    // 数据库名
-	dbConfig["DATABASE_HEAD"] = ""   // 库名头
-	dbConfig["DATABASE_FOOTER"] = "" // 库名尾
-	dbConfig["USER"] = "root"        // 用户名
-	dbConfig["PWD"] = "ch123456"     // 密码
-	dbConfig["CHARSET"] = "utf8"     // 编码
+	dbConfig["HOST"] = "127.0.0.1"     // 主机
+	dbConfig["PORT"] = "3306"          // 端口
+	dbConfig["DATABASE"] = "pineapple" // 数据库名
+	dbConfig["DATABASE_HEAD"] = ""     // 库名头
+	dbConfig["DATABASE_FOOTER"] = ""   // 库名尾
+	dbConfig["USER"] = "root"          // 用户名
+	dbConfig["PWD"] = "swiftwhale2018" // 密码
+	dbConfig["CHARSET"] = "utf8"       // 编码
 
 	return dbConfig
 }
